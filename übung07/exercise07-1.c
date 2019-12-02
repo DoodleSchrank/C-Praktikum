2f285dde3e462ee4f758c9d789ae647e50a09da5
Jonas Bögle <jonas@boegle.me>
2019-12-02 12:52:03 +0100

    add 07

---
+++ übung07/exercise07-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void print2dArray(size_t height, size_t width, /*TODO*/) {
+	//TODO
+}
+
+int main() {
+	const size_t width = 4, height = 3;
+	double data[height][width];
+	for(size_t y = 0; y < height; y++) {
+		for(size_t x = 0; x < width; x++) {
+			data[y][x] = 0;
+			if(y == 1 && x == 1) data[y][x] = 4;
+			if(y == 1 && x == 2) data[y][x] = 2;
+		}
+	}
+
+	print2dArray(height, width, data);
+}
