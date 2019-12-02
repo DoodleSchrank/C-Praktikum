6980aacd5a912f32373ca5b872f963f9f7a72932
Jonas Bögle <jonas@boegle.me>
2019-12-02 20:12:11 +0100

    fix indentation in headers

--- übung07/2dArrayCall.h
+++ übung07/2dArrayCall.h
@@ -3,11 +3,11 @@
 void print2dArray(size_t height, size_t width, double data[height][width]) {
 	for(size_t x = 0; x < width; x++)
 	{
-	    for(size_t y = 0; y < height; y++)
-        {
-	        printf("%f ", data[y][x]);
-        }
-	    printf("\n");
+		for(size_t y = 0; y < height; y++)
+		{
+			printf("%f ", data[y][x]);
+		}
+		printf("\n");
 	}
 }
 
