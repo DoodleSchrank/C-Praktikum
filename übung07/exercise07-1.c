7214749c840c83a00e7f6e8cb286cb15e56e0958
Jonas Bögle <jonas@boegle.me>
2019-12-02 13:12:56 +0100

    implement 07-1

--- übung07/exercise07-1.c
+++ übung07/exercise07-1.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-void print2dArray(size_t height, size_t width, /*TODO*/) {
-	//TODO
+void print2dArray(size_t height, size_t width, double data[height][width]) {
+	for(size_t y = 0; y < height; y++) {
+		for(size_t x = 0; x < width; x++) {
+			printf("%f ", data[y][x]);
+		}
+		printf("\n");
+	}
 }
 
 int main() {
