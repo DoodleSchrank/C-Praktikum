21bff190b66d617fd5bade3504c2aab96c964736
Jonas Bögle <jonas@boegle.me>
2020-02-02 00:21:37 +0100

    old refactor

--- übung12/data.c
+++ übung12/data.c
@@ -10,32 +10,28 @@ struct data
 	short is_string;
 	size_t length;
 	unsigned int ref_count;
-	char* const content;
+	char** content;
 };
 
 /* "content" is a null-terminated string. */
 data* data_new_string (char const* content)
 {
-	size_t bsize = strlen(content);
-	data *dat = malloc(sizeof(data) + sizeof(char) * (bsize - 1));
-
+	data *dat = data_new_blob(content, strlen(content)+1);
 	dat->is_string = 1;
-	dat->length = bsize;
-	dat->ref_count = 1;
-	strcpy((char*) &dat->content, (char*) &content);
-
 	return dat;
 }
 
 /* "content" is a blob of length "length". */
 data* data_new_blob (char const* content, unsigned int length)
 {
-	data *dat = malloc(sizeof(data) + sizeof(char) * (length - 1));
+	data *dat = malloc(sizeof(data));
 
-	dat->is_string = 1;
+	dat->is_string = 0;
 	dat->length = length;
 	dat->ref_count = 1;
-	strcpy((char*) &dat->content, (char*) &content);
+	dat->content = malloc(length);
+
+	memcpy(dat->content, content, length);
 
 	return dat;
 }
@@ -43,7 +39,7 @@ data* data_new_blob (char const* content, unsigned int length)
 data* data_ref (data* data)
 {
 	data->ref_count++;
-	printf("ref: %p\n", data);
+	printf("ref: %p\n", (void *)data);
 	return data;
 }
 
@@ -51,7 +47,11 @@ data* data_ref (data* data)
 void data_unref (data* data)
 {
 	data->ref_count--;
-	if(data->ref_count == 0) free(data);
+	if(data->ref_count == 0)
+	{
+		free(data->content);
+		free(data);
+	}
 }
 
 /* Returns a newly-allocated string that must be freed by the caller. */
@@ -60,17 +60,20 @@ char* data_as_string (data const* data)
 	char* prefix = data->is_string ? "String: " : "Blob: ";
 	char* data_string = malloc((strlen(prefix)+data->length + 2)*sizeof(char));
 	if(data->is_string)
-		sprintf(data_string,"%s%s", prefix, data->content);
+		sprintf(data_string,"%s%s", prefix, *data->content);
 	else
-		sprintf(data_string,"%s%p", prefix, &data->content);
-	printf("string pointer: %p\nstring: %s\n", data, data->content);
+		sprintf(data_string,"%s%p", prefix, (void *)data->content);
+	printf("string pointer: %p\nstring: %s\n", (void *)data, *data->content);
 	return data_string;
 }
 
 unsigned int data_hash (data const* data)
 {
-	unsigned int hash = 0;
-	for (int i = 0; i < (int) data->length;	hash += i * (int) data->content[++i]);
+	unsigned int hash = 5381;
+	for (size_t i = 0; i < data->length; i++)
+	{
+		hash = ((hash << 5) + hash) + (*data->content)[i]; /* hash * 33 + c */
+	}
 	return hash;
 }
 
