7dbdc235194a28361571f01256f7ef4b87326808
Jonas Bögle <jonas@boegle.me>
2020-02-02 04:03:23 +0100

    add more comments

--- übung12/data.c
+++ übung12/data.c
@@ -7,18 +7,26 @@
 
 #include "data.h"
 
+/* data stores either a string or blob. */
 struct data
 {
+	// 1 in case of string, 0 for blob
 	short is_string;
+	// content length; includes null-terminator in case of string
 	size_t length;
+	// reference count
 	unsigned int ref_count;
+	// string/blob
 	char* content;
 };
 
 /* "content" is a null-terminated string. */
 data* data_new_string (char const* content)
 {
+	// length is strlen + 1 because it includes null-terminator;
+	// otherwise we treat strings and blobs equally
 	data* dat = data_new_blob(content, strlen(content)+1);
+	// set data type to string
 	dat->is_string = 1;
 	return dat;
 }
@@ -26,18 +34,23 @@ data* data_new_string (char const* content)
 /* "content" is a blob of length "length". */
 data* data_new_blob (char const* content, unsigned int length)
 {
+	// allocate space for data struct
 	data* dat = malloc(sizeof(data));
 
+	// initialize
 	dat->is_string = 0;
 	dat->length = length;
 	dat->ref_count = 1;
+	// allocate space for content
 	dat->content = malloc(length);
 
+	// copy content to data struct
 	memcpy(dat->content, content, length);
 
 	return dat;
 }
 
+/* Increments the reference count. */
 data* data_ref (data* data)
 {
 	data->ref_count++;
@@ -48,7 +61,8 @@ data* data_ref (data* data)
 void data_unref (data* data)
 {
 	data->ref_count--;
-	if (data->ref_count <= 0)
+	// free content and data if not referenced anymore
+	if (data->ref_count == 0)
 	{
 		free(data->content);
 		free(data);
@@ -60,40 +74,47 @@ char* data_as_string (data const* data)
 {
 	char* str = NULL;
 
-	if (data->is_string)
+	if (data->is_string) // "String: "-prefix + content
 		asprintf(&str, "String: %s", data->content);
-	else
+	else // "Blob: "-prefix + address
 		asprintf(&str, "Blob: %p", data->content);
 	
 	return str;
 }
 
+/* Returns a hash for the content of data. */
 unsigned int data_hash (data const* data)
 {
 	unsigned int hash = 0;
 	for (size_t i = 0; i < data->length; i++)
 	{
-		hash = data->content[i] + 31*hash;
+		// adding the char makes sure that the value affects the hash
+		// and to ensure the position also has an effect we multiply the hash
+		hash = 31*hash + data->content[i];
 	}
 	return hash;
 }
 
+/* Compares two data structs. */
 int data_cmp (data const* a, data const* b)
 {
-	if (a->length > b->length)
-		return 1;
-	else if (a->length < b->length)
-		return -1;
+	// compare by length
+	if (a->length < b->length)
+		return -1; // a is shorter than b
+	else if (a->length > b->length)
+		return 1; // a is longer than b
 	else
 	{
+		// length is equal, compare lexicographically
 		for (size_t i = 0; i < a->length; i++)
 		{
-			if (a->content[i] > b->content[i])
-				return 1;
-			else if (a->content[i] < b->content[i])
-				return -1;
+			if (a->content[i] < b->content[i])
+				return -1; // a comes before b
+			else if (a->content[i] > b->content[i])
+				return 1; // a comes after b
 		}
 	}
 
+	// otherwiese, both length and content are equal
 	return 0;
 }
